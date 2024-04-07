8935b513ecc43f6d327901dd0d180159d3be88ad
knjprojects <joshthereactdev@gmail.com>
2024-04-07 08:19:30 -0400

    Major update to models, static files , views and controllers

--- App/views/index.py
+++ App/views/index.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.models import db
+from App.database import db
 from App.controllers import create_user, create_book, review_book
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
