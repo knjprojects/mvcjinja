3fffbbdc0bbd8603497dcfa817be04e79313f897
knjprojects <joshthereactdev@gmail.com>
2024-04-07 23:53:39 -0400

    books jinja link loads book, from /route, so init should work as well, now we test reviewbook function but it clearly makes render.com crash but fixed ith with adding review_views to views ' init.py added indent to user definitions incase thats why review_book not recognizing, testing the review_book definitions for user now, all other relationships work asexpected

--- App/models/book.py
+++ App/models/book.py
@@ -6,21 +6,21 @@ class Book(db.Model):
   publisher = db.Column(db.String(255),nullable=False)
   reviews_count = db.Column(db.Integer, default=0)
   reviews = db.relationship('Review', backref='book', lazy=True)
-def __init__(self, name, author, publisher):
-  self.name = name
-  self.author = author
-  self.publisher = publisher
-  self.reviews_count=0
-  
-def __repr__(self):
-    return f'<Book {self.id} : {self.name} author {self.author} publisher {self.publisher}>'
-  
-def get_json(self):
-  return {
-    'id': self.id,
-    'name': self.name,
-    'author': self.author,
-    'publisher': self.publisher,
-    'reviews_count': self.reviews_count,
+  def __init__(self, name, author, publisher):
+    self.name = name
+    self.author = author
+    self.publisher = publisher
+    self.reviews_count=0
     
-}
+  def __repr__(self):
+      return f'<Book {self.id} : {self.name} author {self.author} publisher {self.publisher}>'
+    
+  def get_json(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'author': self.author,
+      'publisher': self.publisher,
+      'reviews_count': self.reviews_count,
+      
+  }
