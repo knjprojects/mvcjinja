22020847e830f9a5563d305b681277fad6c2b9f5
knjprojects <joshthereactdev@gmail.com>
2024-04-08 01:00:17 -0400

    books jinja link loads book, from /route, so init should work as well, now we test reviewbook function but it clearly makes render.com crash but fixed ith with adding review_views to views ' init.py added indent to user definitions incase thats why review_book not recognizing, testing the review_book definitions for user now, all other relationships work asexpected

--- App/views/index.py
+++ App/views/index.py
@@ -10,7 +10,7 @@ def index_page():
     db.drop_all()
     db.create_all()
     bob=create_user('bob', 'bobpass')
-    create_book('The Hobbit', 'J.R.R. Tolkien', 'George Allen & Unwin','https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FThe_Hobbit_%2528film_series%2529&psig=AOvVaw3lI_iZvup8UOSgMlCbAjzf&ust=1712638446482000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLi-hsTpsYUDFQAAAAAdAAAAABAE')
+    create_book('The Hobbit', 'J.R.R. Tolkien', 'George Allen & Unwin','https://en.wikipedia.org/wiki/File:The_Hobbit_trilogy_dvd_cover.jpg')
     #create_review(1,1,3,'Great!')
     bob.review_book(1, 3,'A great book!')
     return render_template('index.html')
