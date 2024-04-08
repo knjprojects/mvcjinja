8d5f3cf3e4f32c75863566b961440a6589f67cdf
knjprojects <joshthereactdev@gmail.com>
2024-04-07 23:37:33 -0400

    books jinja link loads book, from /route, so init should work as well, now we test reviewbook function but it clearly makes render.com crash but fixed ith with adding review_views to views ' init.py but may have caused another eror, with review getJson referencing bookname etc through review.book.name

--- App/models/review.py
+++ App/models/review.py
@@ -19,9 +19,9 @@ class Review(db.Model):
   def get_json(self):
     return{
       'id': self.id,
-      'user_id': self.user.id,
-      'book_name': self.book.name,
-      'book_id': self.book.id,
+      #'user_id': self.user.id,
+      #'book_name': self.book.name,
+      #'book_id': self.book.id,
       'rating': self.rating,
       'reviewtext': self.reviewtext
     }
